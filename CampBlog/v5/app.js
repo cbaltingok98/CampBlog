30709ae76b968bf25974b54849747f9768de06a2
Celik Berk Altingok <cbaltingok@gmail.com>
2019-09-08 23:18:20 -0700

    Add files via upload

---
+++ CampBlog/v5/app.js
@@ -0,0 +1,108 @@
+const express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    Campground = require("./models/campground"),
+    Comment = require("./models/comment"),
+    seedDB = require("./seeds");
+
+
+
+
+mongoose.connect('mongodb://localhost/yelp_camp_v4', { useNewUrlParser: true });
+app.use(bodyParser.urlencoded({ extended: true }));;
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+seedDB();
+
+app.get("/", function (req, res) {
+    res.render("landing");
+});
+
+app.get("/campgrounds", function (req, res) {
+    //Get all the campgrounds from DB
+    Campground.find({}, function (err, allCampgrounds) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("campgrounds/index", { campgrounds: allCampgrounds });
+        }
+    });
+
+});
+
+app.post("/campgrounds", function (req, res) {
+    let name = req.body.name;
+    let image = req.body.image;
+    let description = req.body.description;
+    let newCampground = { name: name, image: image, description: description };
+    // Create a new campground and save to DB
+    Campground.create(newCampground, function (err, newlyCreated) {
+        if (err) {
+            console.log(err);
+        } else {
+            //redirect back to campgrounds page
+            res.redirect("/campgrounds");
+        }
+    });
+
+
+});
+
+app.get("/campgrounds/new", function (req, res) {
+    res.render("campgrounds/new");
+});
+
+//SHOW - shows more info about one campground
+app.get("/campgrounds/:id", function (req, res) {
+    //find the campground with provided ID
+    Campground.findById(req.params.id).populate("comments").exec(function (err, foundCampground) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(foundCampground);
+            //render show template with that campground
+            res.render("campgrounds/show", { campground: foundCampground });
+        }
+    });
+
+
+});
+
+// =============
+// COMMENTS ROUTES
+//==============
+
+app.get("/campgrounds/:id/comments/new", function(req, res) {
+    Campground.findById(req.params.id, function(err, campground){
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("comments/new", {campground: campground});
+        }
+    });
+    
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+    Campground.findById(req.params.id, function(err, campground){
+        if(err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            Comment.create(req.body.comment, function(err, comment){
+                if(err) {
+                    console.log(err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            });
+        }
+    });
+});
+
+app.listen(3000, '127.0.0.1', function () {
+    console.log("The YelpCamp_V4 Server Has Started!");
+});
